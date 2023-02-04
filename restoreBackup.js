c68cead1a06d50aff38c82268ba227ac12bc4859
SamB-C <samson_bc@icloud.com>
2023-02-04 19:54:12 +0000

    Improve convertedPoems.json formatting

--- restoreBackup.js
+++ restoreBackup.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const convertedPoemsBackup = fs.readFileSync('./convertedPoemsBackup.json', {encoding: 'utf-8'});
-fs.writeFile('./convertedPoems.json', convertedPoemsBackup, (err) => {
+fs.writeFile('./convertedPoems.json', JSON.stringify(JSON.parse(convertedPoemsBackup), null, 4), (err) => {
     if (err) {
         console.log("Backup not restored:");
         console.log(err);
